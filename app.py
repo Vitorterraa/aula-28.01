6c88d94c48b89577c626be870375f69fa1f6b49e
Vitor Terra <vterra2008@gmail.com>
2025-01-28 11:21:37 -0300

    mudança de cor, mudança de fotos e mudança de curiosidade criação da pagina principal

--- app.py
+++ app.py
@@ -1,12 +1,58 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
-                                                                                  
-#Aqui ira todas as minhas rotas
+         
+         
+#lista de cores para alterar o fundo do site
 
-app.run()                                                                                 
+lista_cores = ["red",
+               "blue",
+               "green",
+               "yellow",
+               "pink",
+               "purple"]
+
+   
+lista_curiosidades = [
+    "Quase jogou no Real Madrid quando era criança",
+    "Primeiro jogador a marcar gols em competições diferentes no mesmo ano",
+    "Tem uma grande paixão por pôquer",
+    "Seu nome foi inspirado no pai",
+    "Fez um gol mais rápido que muitos jogadores da história"
+]
+
+lista_fotos = [
+    "neymar1.webp",
+    "neymar2.jpg",
+    "neymar3.jfif",
+    "neymar4.jfif",
+    "neymar5.jpg"
+]    
+
+                                                        
+#Aqui ir para todas as minhas rotas
+
+@app.route("/")
+
+def principal():
+    cor_de_fundo = random.choice(lista_cores)
+    curiosidade = random.choice(lista_curiosidades)
+    foto = random.choice(lista_fotos)
+    return render_template("principal.html", cor_de_fundo_html = cor_de_fundo, curiosidade_html = curiosidade, foto_html = foto)
+    
+    
+@app.route("/sobre")
+
+
+def pagina_sobre():
+    cor_de_fundo = random.choice(lista_cores)
+    return render_template("sobre.html", cor_de_fundo_html = cor_de_fundo)
+    
+
+app.run(debug=True)                                                                                 
                                                                                   
-                                                                                  
+                                                                                                                                                      
                                                                                   
                                                                                   
                                                                                   
