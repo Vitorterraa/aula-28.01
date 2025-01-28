992fe2f0d709fe2931f6e16f11bc6028214fb6bb
Leticia Lemes <leticia.lemes4384@gmail.com>
2025-01-28 08:24:11 -0300

    ADD - Estrutura inicial do projeto

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+                                                                                  
+#Aqui ira todas as minhas rotas
+
+app.run()                                                                                 
+                                                                                  
+                                                                                  
+                                                                                  
+                                                                                  
+                                                                                  
+                                                                                  
